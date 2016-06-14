5ad445d93b14c726cb40fecfc2d0dd36a4b014b6
caraclarke <cara.clarke8@gmail.com>
2016-06-14 12:10:24 -0400

    add connect functionality from react-redux
    wire provider embedded react components to Redux store
    extend Film component to get required state from Redux store
    
    use returned function of connect to take Film component as argument to return higher order component -> access Redux store while Film component itself just presents data
    
    mapStateToProps returns object which is substate of global state

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function Films({ films = [] }) {
-  return (
-    <div>
-      {
-        films.map((film, key) => {
-          return <div className="film" key={key}>{film.title}</div>
-        })
-      }
-    </div>
-  )
-}
+  function Films({ films = [] }) {
+    return (
+      <div>
+        {
+          films.map((film, key) => {
+            return <div className="film" key={key}>{film.title}</div>
+          })
+        }
+      </div>
+    )
+  }
 
-export default Films;
+  function mapStateToProps(state) {
+    const films = state.film;
+    return {
+      films
+    }
+  }
+
+export default connect(mapStateToProps)(Films);
